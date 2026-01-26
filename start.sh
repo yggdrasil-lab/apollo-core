f7f44583dcbf845feb128beea7ea724d222c9a6f
raymundmt <mon2x0@gmail.com>
2026-01-26 13:56:47 +1300

    feat(audiobookshelf): add service, split setup scripts, and refactor deploy workflow

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-# Usage: ./scripts/deploy.sh <STACK_NAME> [COMPOSE_FILES...]
-
-# Requirement: Run ./setup_host.sh on the target node first.
-
-./scripts/deploy.sh --skip-build "apollo-core" docker-compose.yml
